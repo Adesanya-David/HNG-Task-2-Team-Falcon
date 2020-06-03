e80ea8d25a96818d6eaeaa5759c4c89aaad8f944
Adesanya-David <62469624+Adesanya-David@users.noreply.github.com>
2020-06-03 16:01:14 +0100

    Create hng03421.py

---
+++ scripts/hng03421.py
@@ -0,0 +1,8 @@
+Full_name ="Adesanya David"
+hng_id = "HNG-03421"
+scripting_lang = "Python" 
+email = " adesanya.tito@gmail.com"
+
+print("Hello World, this is " + full_name + " with  HNGi7 ID :" + hng_id + " using " + language + " for stage 2 task " + email,flush=True);
+
+//I declared variables and called them using the print function. This is a python task"//
